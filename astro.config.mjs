a1e94f664e5e06e89b1c19b08b09bd07688b1d36
Karol Rutkowski <karolrut123@gmail.com>
2025-03-09 19:37:43 +0100

    feat: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 // @ts-check
+import vercelStatic from '@astrojs/vercel/static';
 import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
-
 import icon from 'astro-icon';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-
   integrations: [icon()],
+  adapter: vercelStatic({}),
+  output: 'static',
 });
