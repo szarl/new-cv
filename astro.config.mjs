640fc18d2265f31157578a10fd60d9f284256d6b
Karol Rutkowski <karolrut123@gmail.com>
2025-03-29 20:28:52 +0100

    fix: ensure url is provided for config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-  site: import.meta.env.SITE_URL,
+  site: import.meta.env.SITE_URL || 'https://www.rutkowski-software.com',
   vite: {
     plugins: [tailwindcss()],
   },
