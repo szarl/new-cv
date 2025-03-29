0c2bfd41402a24493b1c8efd6b7169084f127aa2
Karol Rutkowski <karolrut123@gmail.com>
2025-03-29 18:40:55 +0100

    feat: add site url to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: import.meta.env.SITE_URL,
   vite: {
     plugins: [tailwindcss()],
   },
